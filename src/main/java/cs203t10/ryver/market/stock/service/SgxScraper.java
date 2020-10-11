12f97520317b9104567dae2978263cddf1cde1d1
Bryan Lee <bryanmylee@gmail.com>
2020-10-11 19:04:34 +0800

    Separate scraping functionality into own class instance

---
+++ src/main/java/cs203t10/ryver/market/stock/service/SgxScraper.java
@@ -0,0 +1,135 @@
+package cs203t10.ryver.market.stock.service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import cs203t10.ryver.market.stock.Stock;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfAllElementsLocatedBy;
+
+public class SgxScraper {
+
+    public final static String SGX_URL = "https://www.sgx.com/indices/products/sti/";
+
+    WebDriver driver;
+
+    public SgxScraper() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--window-size=800,600");
+        driver = new ChromeDriver(options);
+    }
+
+    public List<Stock> getAllStocks() {
+        Set<Stock> result = new HashSet<>(30);
+        try {
+            driver.get(SGX_URL);
+            // Wait for page data to load.
+            Thread.sleep(800);
+            scrollToTable();
+            hideConsentBanner();
+            // Add stock data that is initially mounted to the DOM.
+            result.addAll(getCurrentStocksFromMountedRows());
+            scrollTableDown();
+            // Give some time for data to hydrate the DOM.
+            Thread.sleep(800);
+            // Add any stock data that has been newly mounted to the DOM.
+            result.addAll(getCurrentStocksFromMountedRows());
+            return List.copyOf(result);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return List.copyOf(result);
+        } finally {
+            driver.quit();
+        }
+    }
+
+    private void scrollToTable() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('sgx-table-list')"
+                + ".scrollIntoView()", "");
+    }
+
+    private void hideConsentBanner() {
+        // Hide consent banner.
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('sgx-consent-banner')"
+                + ".style.display = 'none'", "");
+    }
+
+    /**
+     * Scrape stock data from data table rows which are currently mounted
+     * to the DOM.
+     */
+    private Set<Stock> getCurrentStocksFromMountedRows()
+            throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        List<WebElement> tableRows = wait.until(
+                presenceOfAllElementsLocatedBy(By.cssSelector("sgx-table-row")));
+        return tableRows.stream()
+                .map(row -> getStock(row))
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * Simulate interaction with the interface.
+     *
+     * The scrollbar element must be visible.
+     */
+    private void scrollTableDown() {
+        // Scroll the data table down to render the rest of data required.
+        WebElement scrollBar = driver.findElement(
+                By.cssSelector(".vertical-scrolling-bar"));
+        Actions dragger = new Actions(driver);
+        dragger
+            .moveToElement(scrollBar)
+            .clickAndHold()
+            .moveByOffset(0, 200)
+            .build().perform();
+    }
+
+    private Stock getStock(WebElement row) {
+        String symbol = getSymbol(row);
+        return Stock.builder().symbol(symbol).build();
+    }
+
+    private static WebElement getCellWithId(WebElement row, String id) {
+        String selector = String.format("[data-column-id='%s']", id);
+        return row.findElement(By.cssSelector(selector));
+    }
+
+    private static String getSymbol(WebElement row) {
+        return getCellWithId(row, "nc").getAttribute("innerHTML");
+    }
+
+    private static Double getLastPrice(WebElement row) {
+        String value = getCellWithId(row, "lt").getAttribute("innerHTML");
+        try {
+            return Double.parseDouble(value);
+        } catch (NumberFormatException e) {
+            return 0.0;
+        }
+    }
+
+    private static Integer getTotalVolume(WebElement row) {
+        String value = getCellWithId(row, "vl").getAttribute("innerHTML");
+        value = value.replaceAll(",", "");
+        try {
+            return (int) (Double.parseDouble(value) * 1000);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+}
--- src/main/java/cs203t10/ryver/market/stock/service/StockSgxScrapingService.java
+++ src/main/java/cs203t10/ryver/market/stock/service/StockSgxScrapingService.java
@@ -1,23 +1,10 @@
 package cs203t10.ryver.market.stock.service;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
 import cs203t10.ryver.market.stock.Stock;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfAllElementsLocatedBy;
 
 /**
  * Scrape SGX for updated Straits Time Index data.
@@ -25,121 +12,14 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfAllEle
 @Service
 public class StockSgxScrapingService implements StockService {
 
-    public final static String SGX_URL = "https://www.sgx.com/indices/products/sti/";
-
     public StockSgxScrapingService() {
         System.setProperty("webdriver.chrome.driver", "lib/chromedriver");
     }
 
     @Override
     public List<Stock> getAllStocks() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--window-size=800,600");
-        WebDriver driver = new ChromeDriver(options);
-        Set<Stock> result = new HashSet<>(30);
-        try {
-            driver.get(SGX_URL);
-            // Wait for page data to load.
-            Thread.sleep(800);
-            scrollToTable(driver);
-            hideConsentBanner(driver);
-            // Add stock data that is initially mounted to the DOM.
-            result.addAll(getCurrentStocksFromMountedRows(driver));
-            scrollTableDown(driver);
-            // Give some time for data to hydrate the DOM.
-            Thread.sleep(800);
-            // Add any stock data that has been newly mounted to the DOM.
-            result.addAll(getCurrentStocksFromMountedRows(driver));
-            return List.copyOf(result);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return List.copyOf(result);
-        } finally {
-            System.out.println("SIZE: " + result.size());
-            driver.quit();
-        }
-    }
-
-    private void scrollToTable(WebDriver driver) {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("document.querySelector('sgx-table-list')"
-                + ".scrollIntoView()", "");
-    }
-
-    private void hideConsentBanner(WebDriver driver) {
-        // Hide consent banner.
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("document.querySelector('sgx-consent-banner')"
-                + ".style.display = 'none'", "");
-    }
-
-    /**
-     * Scrape stock data from data table rows which are currently mounted
-     * to the DOM.
-     */
-    private Set<Stock> getCurrentStocksFromMountedRows(WebDriver driver)
-            throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        List<WebElement> tableRows = wait.until(
-                presenceOfAllElementsLocatedBy(By.cssSelector("sgx-table-row")));
-        return tableRows.stream()
-                .map(StockSgxScrapingService::getStock)
-                .collect(Collectors.toSet());
-    }
-
-    /**
-     * Simulate interaction with the interface.
-     *
-     * The scrollbar element must be visible.
-     */
-    private void scrollTableDown(WebDriver driver) {
-        // Scroll the data table down to render the rest of data required.
-        WebElement scrollBar = driver.findElement(
-                By.cssSelector(".vertical-scrolling-bar"));
-        Actions dragger = new Actions(driver);
-        dragger
-            .moveToElement(scrollBar)
-            .clickAndHold()
-            .moveByOffset(0, 200)
-            .build().perform();
-    }
-
-    private static Stock getStock(WebElement row) {
-        return null;
-        // return Stock.builder()
-        //         .symbol(getSymbol(row))
-        //         .lastPrice(getLastPrice(row))
-        //         .totalVolume(getTotalVolume(row))
-        //         .build();
-    }
-
-    private static WebElement getCellWithId(WebElement row, String id) {
-        String selector = String.format("[data-column-id='%s']", id);
-        return row.findElement(By.cssSelector(selector));
-    }
-
-    private static String getSymbol(WebElement row) {
-        return getCellWithId(row, "nc").getAttribute("innerHTML");
-    }
-
-    private static Double getLastPrice(WebElement row) {
-        String value = getCellWithId(row, "lt").getAttribute("innerHTML");
-        try {
-            return Double.parseDouble(value);
-        } catch (NumberFormatException e) {
-            return 0.0;
-        }
-    }
-
-    private static Integer getTotalVolume(WebElement row) {
-        String value = getCellWithId(row, "vl").getAttribute("innerHTML");
-        value = value.replaceAll(",", "");
-        try {
-            return (int) (Double.parseDouble(value) * 1000);
-        } catch (NumberFormatException e) {
-            return 0;
-        }
+        SgxScraper scraper = new SgxScraper();
+        return scraper.getAllStocks();
     }
 
 }
