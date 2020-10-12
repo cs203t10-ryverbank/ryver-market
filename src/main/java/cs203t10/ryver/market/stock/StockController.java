39d3c0faf255563e8806344ada5909e2b54c52b9
Bryan Lee <bryanmylee@gmail.com>
2020-10-12 18:31:59 +0800

    Use StockRecordView for API endpoint

--- src/main/java/cs203t10/ryver/market/stock/StockController.java
+++ src/main/java/cs203t10/ryver/market/stock/StockController.java
@@ -1,9 +1,12 @@
 package cs203t10.ryver.market.stock;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import cs203t10.ryver.market.stock.view.StockRecordView;
 
 @RestController
 public class StockController {
@@ -12,8 +15,11 @@ public class StockController {
     StockRecordService stockRecordService;
 
     @GetMapping("/stocks")
-    public List<StockRecord> getAllStockRecords() {
-        return stockRecordService.getAllLatestStockRecords();
+    public List<StockRecordView> getAllLatestStockRecords() {
+        List<StockRecord> latestStockRecords = stockRecordService.getAllLatestStockRecords();
+        return latestStockRecords.stream()
+                .map(StockRecordView::fromRecordAskBid)
+                .collect(Collectors.toList());
     }
 
 }
