0b080639c670e10ecd577a1a479c3a85d376551f
Bryan Lee <bryanmylee@gmail.com>
2020-10-15 00:07:14 +0800

    Implement stock record repo tests

--- src/test/java/cs203t10/ryver/market/stock/StockRecordRepositoryTest.java
+++ src/test/java/cs203t10/ryver/market/stock/StockRecordRepositoryTest.java
@@ -1,19 +1,17 @@
 package cs203t10.ryver.market.stock;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Date;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-// @ExtendWith(MockitoExtension.class)
 @DataJpaTest
 public class StockRecordRepositoryTest {
 
@@ -26,44 +24,60 @@ public class StockRecordRepositoryTest {
     @Autowired
     TestEntityManager entityManager;
 
+    Date firstDate = new Date(1602321010000L);
+    Date secondDate = new Date(1602324610000L);
+
+    StockRecord a1_1 = StockRecord.builder()
+            .stock(new Stock("A1")).submittedDate(firstDate)
+            .price(1.0).totalVolume(1000).build();
+    // Same price, different dates
+    StockRecord b2_1 = StockRecord.builder()
+            .stock(new Stock("B2")).submittedDate(firstDate)
+            .price(2.0).totalVolume(2000).build();
+    StockRecord b2_2 = StockRecord.builder()
+            .stock(new Stock("B2")).submittedDate(secondDate)
+            .price(2.0).totalVolume(1500).build();
+    // Same dates, different price
+    StockRecord c3_1 = StockRecord.builder()
+            .stock(new Stock("C3")).submittedDate(firstDate)
+            .price(3.0).totalVolume(3000).build();
+    StockRecord c3_2 = StockRecord.builder()
+            .stock(new Stock("C3")).submittedDate(firstDate)
+            .price(2.5).totalVolume(2500).build();
+
+
     @BeforeEach
     public void saveInitialRecords() {
-        Date firstDate = new Date(1602321010000L);
-        Date secondDate = new Date(1602324610000L);
         stockRepo.saveAll(List.of(
                 new Stock("A1"),
                 new Stock("B2"),
                 new Stock("C3")
         ));
-        stockRecordRepo.saveAll(List.of(
-                StockRecord.builder()
-                    .stock(new Stock("A1")).submittedDate(firstDate)
-                    .price(1.0).totalVolume(1000).build(),
-                // Same price, different dates
-                StockRecord.builder()
-                    .stock(new Stock("B2")).submittedDate(firstDate)
-                    .price(2.0).totalVolume(2000).build(),
-                StockRecord.builder()
-                    .stock(new Stock("B2")).submittedDate(secondDate)
-                    .price(2.0).totalVolume(1500).build(),
-                // Same dates, different price
-                StockRecord.builder()
-                    .stock(new Stock("C3")).submittedDate(firstDate)
-                    .price(3.0).totalVolume(3000).build(),
-                StockRecord.builder()
-                    .stock(new Stock("A1")).submittedDate(firstDate)
-                    .price(2.5).totalVolume(2500).build()
-        ));
+        stockRecordRepo.saveAll(List.of(a1_1, b2_1, b2_2, c3_1, c3_2));
     }
 
     @AfterEach
     public void resetRecords() {
+        stockRepo.deleteAll();
         stockRecordRepo.deleteAll();
     }
 
     @Test
     public void findAllBySymbolTest() {
+        List<StockRecord> b2Result = stockRecordRepo.findAllBySymbol("B2");
+        assertEquals(List.of(b2_1, b2_2), b2Result);
+    }
 
+    @Test
+    public void findAllBySymbolNonSuchStockTest() {
+        List<StockRecord> emptyResult = stockRecordRepo.findAllBySymbol("Z26");
+        assertEquals(List.of(), emptyResult);
+    }
+
+    @Test
+    public void findAllLatestPerStockTest() {
+        List<StockRecord> latestResult = stockRecordRepo.findAllLatestPerStock();
+        assertEquals(List.of(a1_1, b2_2, c3_1, c3_2), latestResult);
     }
 
 }
