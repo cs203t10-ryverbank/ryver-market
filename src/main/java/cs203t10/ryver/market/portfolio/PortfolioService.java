4158cc5c41520255476879cac553c3701e1440f7
Bryan Lee <bryanmylee@gmail.com>
2020-10-15 01:47:12 +0800

    Remove public on interface
    Interface stubs are public by default.

--- src/main/java/cs203t10/ryver/market/portfolio/PortfolioService.java
+++ src/main/java/cs203t10/ryver/market/portfolio/PortfolioService.java
@@ -3,14 +3,14 @@ package cs203t10.ryver.market.portfolio;
 import java.util.List;
 
 import cs203t10.ryver.market.trade.Trade;
-import cs203t10.ryver.market.portfolio.Asset;
 
 public interface PortfolioService {
-    public Portfolio createPortfolio(Integer customerId);
-    public Portfolio findByCustomerId(Integer customerId);
-    public List<Asset> findAssetsByCustomerId(Integer customerId);
-    public Asset findAssetByCustomerIdAndCode(Integer customerId, String code);
-    public Portfolio addNewStockToAssets(Integer customerId, Asset asset);
-    public Portfolio addToAssets(Trade trade);
-    public Portfolio deductFromAssets(Trade trade);
+    Portfolio createPortfolio(Integer customerId);
+    Portfolio findByCustomerId(Integer customerId);
+    List<Asset> findAssetsByCustomerId(Integer customerId);
+    Asset findAssetByCustomerIdAndCode(Integer customerId, String code);
+    Portfolio addNewStockToAssets(Integer customerId, Asset asset);
+    Portfolio addToAssets(Trade trade);
+    Portfolio deductFromAssets(Trade trade);
 }
+
