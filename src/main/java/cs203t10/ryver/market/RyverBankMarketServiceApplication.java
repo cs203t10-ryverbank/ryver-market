37c6a30921e8db24f860b01f30fd04b3d9155201
Bryan Lee <bryanmylee@gmail.com>
2020-10-14 23:20:41 +0800

    Remove RestTemplate bean

--- src/main/java/cs203t10/ryver/market/RyverBankMarketServiceApplication.java
+++ src/main/java/cs203t10/ryver/market/RyverBankMarketServiceApplication.java
@@ -2,12 +2,9 @@ package cs203t10.ryver.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import io.swagger.annotations.ApiOperation;
 
@@ -24,11 +21,6 @@ public class RyverBankMarketServiceApplication {
     @ApiOperation(value = "Check the service name")
     public String getRoot() {
         return "ryver-market service";
-    }
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
     }
 
 }
