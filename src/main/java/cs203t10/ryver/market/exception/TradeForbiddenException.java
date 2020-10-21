80b03fd68bdd6088fb9b0a7adbd44202e13e626d
christopherlim98 <christopherlim98@gmail.com>
2020-10-21 16:39:32 +0800

    Refactor exceptions

---
+++ src/main/java/cs203t10/ryver/market/exception/TradeForbiddenException.java
@@ -0,0 +1,14 @@
+package cs203t10.ryver.market.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Trade rejected, quantity must be multiple of 100.")
+public  class TradeForbiddenException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public TradeForbiddenException(Integer quantity) {
+        super(String.format("Trade with quantity: %s must be multiple of 100", quantity));
+    }
+
+}
