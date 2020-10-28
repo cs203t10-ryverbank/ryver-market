d7e1c0f3a518e1a869f2a35ca420a7151abe6447
Bryan Lee <bryanmylee@gmail.com>
2020-10-28 13:50:26 +0800

    Lint exception classes

--- src/main/java/cs203t10/ryver/market/exception/TradeInvalidDateException.java
+++ src/main/java/cs203t10/ryver/market/exception/TradeInvalidDateException.java
@@ -4,11 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class TradeInvalidDateException extends RuntimeException {
+public final class TradeInvalidDateException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
-    public TradeInvalidDateException(String time) {
+    public TradeInvalidDateException(final String time) {
         super(String.format("Current time is: %s. Please make a trade on weekdays, 9am to 5pm.", time));
     }
 
 }
+
--- src/main/java/cs203t10/ryver/market/exception/TradeNotFoundException.java
+++ src/main/java/cs203t10/ryver/market/exception/TradeNotFoundException.java
@@ -4,11 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public  class TradeNotFoundException extends RuntimeException {
+public final class TradeNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
-    public TradeNotFoundException(Integer id) {
+    public TradeNotFoundException(final Integer id) {
         super(String.format("Trade with id: %s not found", id));
     }
 
 }
+
