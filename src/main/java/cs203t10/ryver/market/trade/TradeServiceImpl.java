82aa7d304dfe3043f99e9e4a06de077139950c1b
Bryan Lee <bryanmylee@gmail.com>
2020-10-28 16:09:00 +0800

    Separate scheduled services

--- src/main/java/cs203t10/ryver/market/trade/TradeServiceImpl.java
+++ src/main/java/cs203t10/ryver/market/trade/TradeServiceImpl.java
@@ -7,12 +7,9 @@ import java.time.LocalTime;
 import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -387,39 +384,6 @@ public final class TradeServiceImpl implements TradeService {
         LocalDateTime localDate = LocalDateTime.now();
         Date todayDate = Date.from(localDate.atZone(defaultZoneId).toInstant());
         return todayDate;
-    }
-
-    // TODO: Verify this works on AWS
-    @Scheduled(cron = "0 0 17 * * MON-FRI", zone = "Asia/Singapore")
-    public void closeMarket() {
-        // Cron expression: close market at 5pm from Monday to Friday.
-        List<Trade> tradeList = tradeRepo.findAll();
-        Set<List<Integer>> customerAccountSet = new HashSet<>();
-
-        // Close trades for all trades in the tradeList
-        for (Trade trade : tradeList) {
-            // TODO: Implement tradeRepo.findAllNonExpired() instead.
-            if (trade.getStatus().equals(Status.EXPIRED)) {
-                continue;
-            }
-
-            // Expire trades.
-            trade.setStatus(Status.EXPIRED);
-            tradeRepo.save(trade);
-
-            Integer customerId = trade.getCustomerId();
-            Integer accountId = trade.getAccountId();
-            // Set will only store unique instances of { customerId, accountId }.
-            customerAccountSet.add(List.of(customerId, accountId));
-        }
-
-        // For each { customerId, accountId } pair, reset available balance.
-        for (List<Integer> pair : customerAccountSet) {
-            Integer customerId = pair.get(0);
-            Integer accountId = pair.get(1);
-            // Reset balance using FTS
-            fundTransferService.resetAvailableBalance(customerId, accountId);
-        }
     }
 
     @Override
